3c5b08c5cbab5822fde12deea0be99fb1c824837
Jade1724 <haruka.i1724@gmail.com>
2023-02-28 16:50:12 +1300

    Created AboutPage and link from the home.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
+import Link from 'next/link';
+
 function HomePage() {
   return (
-    <main>
-      <h1>My Blog</h1>
-    </main>
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <h1>My Blog</h1>
+      </main>
+    </>
   );
 }
 
