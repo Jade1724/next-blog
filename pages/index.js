66fa3d4965788d580d79baa8eaad686023d7cf7f
Jade1724 <haruka.i1724@gmail.com>
2023-03-01 15:49:49 +1300

    Implemented getStaticProps method for first-post page.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 function HomePage() {
   return (
@@ -9,9 +10,11 @@ function HomePage() {
       <main>
         <h1>My Blog</h1>
         <ul>
-          <li>one</li>
-          <li>two</li>
-          <li>three</li>
+          <li>
+            <Link href="/posts/first-post">
+              First Post
+            </Link>
+          </li>
         </ul>
       </main>
     </>
---
+++ pages/posts/first-post.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+
+export async function getStaticProps() {
+    console.log('[FirstPostPage] getStaticProps()');
+  return {
+    props: {
+      post: {
+        title: "First post",
+        body: "My first post, as static props",
+      },
+    },
+  };
+}
+function FirstPostPage({ post }) {
+  console.log("[FirstPostPage] render: ", post);
+  return (
+    <>
+      <Head>
+        <title>{post.title} - My Blog</title>
+      </Head>
+      <main>
+        <h1>{post.title}</h1>
+        <p>{post.body}</p>
+      </main>
+    </>
+  );
+}
+
+export default FirstPostPage;
